d2303e1d5dab67d9448eef41146e1e804da9236a
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-01-17 14:08:38 +0100

    modules and server setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+dotenv.config()
+
+const app= express ()
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use(cookieParser());
+// Enable CORS
+app.use(cors());
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+}
+)
+//Routes
+
