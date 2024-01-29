0ec146c479b1eb96365060ecfa1a4821e3680dc3
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-01-29 13:53:32 +0100

    Update Header

--- client side/src/components/Header.jsx
+++ client side/src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         type="button"
         className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
       >
-        Contact
+        Sign up
       </button>
             </div>
         </div>
