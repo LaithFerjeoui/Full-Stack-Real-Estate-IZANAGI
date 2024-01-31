ef80b586b006f7ccf1522f44176819418cf9c02e
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-01-31 11:48:07 +0100

    testing

--- client side/src/components/Hero.jsx
+++ client side/src/components/Hero.jsx
@@ -2,30 +2,33 @@ import React from "react";
 import CountUp from "react-countup";
 import { ImLocation2 } from "react-icons/im";
 import { motion } from "framer-motion";
-
+import "./Hero.css";
 const Hero = () => {
   return (
     <section className="text-white pb-4 pt-8 sm:pt-0 ">
       <div className="paddings innerWidth flexHero ">
-        <div className="flexColStart gap-12">
-          <div className="relative z-1 ">
-            <div className="h-16 w-16 bg-gradient-to-br from-orange-500 to-orange-300 absolute rounded-full right-[28%] top-[-10%] z-0" />
-            <motion.h1 
-            initial={{ opacity: 0, y: -9 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 3 , type:"spring"}}
-            className="font-semibold text-5xl sm:text-6xl  mb-11  text-transparent bg-clip-text bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600">
-              Discover <br /> Most Suitable <br /> Property
+      <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <motion.h1
+            initial={{ y: "2rem", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "ease-in",
+            }}
+            >
+              Discover <br />
+              Most Suitable
+              <br /> Property
             </motion.h1>
-            <div className="flexColStart secondaryText mb-11">
-              <span>
-                Find a variety of properties that suit you very easilty
-              </span>
-              <span>
-                Forget all difficulties in finding a residence for you.
-              </span>
-            </div>
-            <div className="bg-white w-[100%] rounded-md border-solid border-[3px] border-gray-400  px-2 py-1 flex justify-between items-center mb-11 ">
+          </div>
+          <div className="flexColStart secondaryText flexhero-des">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+
+          <div className="bg-white w-[100%] rounded-md border-solid border-[3px] border-gray-400  px-2 py-1 flex justify-between items-center mb-11 ">
               <ImLocation2 size={23} color="var(--blue)" />
               <input
                 type="text"
@@ -38,26 +41,25 @@ const Hero = () => {
                 Search
               </button>
             </div>
-          </div>
-          <div className="w-[100%] flex justify-center items-center gap-3 flex-wrap">
-            <div className="flexColCenter">
-              <span className="text-2xl sm:text-4xl">
-                <CountUp start={900} end={1100} duration={4} />
-                <span className="text-orange-400">+</span>
+
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
               </span>
-              <span className="secondaryText">Premium Products</span>
+              <span className="secondaryText">Premium Product</span>
             </div>
-            <div className="flexColCenter">
-              <span className="text-2xl sm:text-4xl">
-                <CountUp start={500} end={700} duration={4} />
-                <span className="text-orange-400">+</span>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} /> <span>+</span>
               </span>
               <span className="secondaryText">Happy Customer</span>
             </div>
-            <div className="flexColCenter">
-              <span className="text-2xl sm:text-4xl">
-                <CountUp start={1} end={11} duration={4} />
-                <span className="text-orange-400">+</span>
+
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp end={28} /> <span>+</span>
               </span>
               <span className="secondaryText">Awards Winning</span>
             </div>
