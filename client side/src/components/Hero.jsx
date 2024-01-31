5d8511d66636c401cf1382a4798e825babcb8a58
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-01-31 11:39:33 +0100

    update hero

--- client side/src/components/Hero.jsx
+++ client side/src/components/Hero.jsx
@@ -6,18 +6,18 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className="text-white pb-4 pt-8 sm:pt-0 ">
-      <div className="paddings  innerWidth flexHero ">
-        <div className="flexColStart ">
+      <div className="paddings innerWidth flexHero ">
+        <div className="flexColStart gap-12">
           <div className="relative z-1 ">
             <div className="h-16 w-16 bg-gradient-to-br from-orange-500 to-orange-300 absolute rounded-full right-[28%] top-[-10%] z-0" />
             <motion.h1 
             initial={{ opacity: 0, y: -9 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 3 , type:"spring"}}
-            className="font-semibold text-5xl sm:text-6xl flexStart mb-11 relative z-1 text-transparent bg-clip-text bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600">
+            className="font-semibold text-5xl sm:text-6xl  mb-11  text-transparent bg-clip-text bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600">
               Discover <br /> Most Suitable <br /> Property
             </motion.h1>
-            <div className="flexColStart text-neutral-500 mb-11">
+            <div className="flexColStart secondaryText mb-11">
               <span>
                 Find a variety of properties that suit you very easilty
               </span>
@@ -25,7 +25,7 @@ const Hero = () => {
                 Forget all difficulties in finding a residence for you.
               </span>
             </div>
-            <div className="bg-white rounded-md border-solid border-[3px] border-gray-400  px-2 py-1 flex justify-between items-center mb-11 ">
+            <div className="bg-white w-[100%] rounded-md border-solid border-[3px] border-gray-400  px-2 py-1 flex justify-between items-center mb-11 ">
               <ImLocation2 size={23} color="var(--blue)" />
               <input
                 type="text"
