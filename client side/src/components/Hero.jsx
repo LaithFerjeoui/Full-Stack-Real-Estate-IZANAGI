8ae2ae41e8b5c3cfc5a9787ce19f176e179a4929
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-01-31 11:58:44 +0100

    updated

--- client side/src/components/Hero.jsx
+++ client side/src/components/Hero.jsx
@@ -1,77 +1,69 @@
-import "./Hero.css";
-import { HiLocationMarker } from "react-icons/hi";
+import React from "react";
 import CountUp from "react-countup";
+import { ImLocation2 } from "react-icons/im";
+import { HiLocationMarker } from "react-icons/hi";
+import "./Hero.css";
 import { motion } from "framer-motion";
+import "./Hero.css";
 const Hero = () => {
   return (
-    <section className="hero-wrapper">
-      <div className="paddings innerWidth flexCenter hero-container">
-        {/* left side */}
-        <div className="flexColStart hero-left">
-          <div className="hero-title">
-            <div className="orange-circle" />
-            <motion.h1
-            initial={{ y: "2rem", opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{
-              duration: 2,
-              type: "ease-in",
-            }}
-            >
-              Discover <br />
-              Most Suitable
-              <br /> Property
+    <section className="text-white pb-4 pt-8 sm:pt-0 ">
+      <div className="paddings innerWidth flexHero ">
+        <div className="flexColStart gap-12">
+          <div className="relative z-1 ">
+            <div className="h-16 w-16 bg-gradient-to-br from-orange-500 to-orange-300 absolute rounded-full right-[28%] top-[-10%] z-0" />
+            <motion.h1 
+            initial={{ opacity: 0, y: -9 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 3 , type:"spring"}}
+            className="font-semibold text-5xl sm:text-6xl  mb-11  text-transparent bg-clip-text bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600">
+              Discover <br /> Most Suitable <br /> Property
             </motion.h1>
-          </div>
-          <div className="flexColStart secondaryText flexhero-des">
-            <span>Find a variety of properties that suit you very easilty</span>
-            <span>Forget all difficulties in finding a residence for you</span>
-          </div>
-
-          <div className="flexCenter search-bar">
+            <div className="flexColStart secondaryText mb-11">
+              <span>
+                Find a variety of properties that suit you very easilty
+              </span>
+              <span>
+                Forget all difficulties in finding a residence for you.
+              </span>
+            </div>
+            <div className="flexCenter search-bar">
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
           </div>
-
-          <div className="flexCenter stats">
-            <div className="flexColCenter stat">
-              <span>
-                <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
+          </div>
+          <div className="w-[100%] flex justify-center items-center gap-3 flex-wrap">
+            <div className="flexColCenter">
+              <span className="text-2xl sm:text-4xl">
+                <CountUp start={900} end={1100} duration={4} />
+                <span className="text-orange-400">+</span>
               </span>
-              <span className="secondaryText">Premium Product</span>
+              <span className="secondaryText">Premium Products</span>
             </div>
-
-            <div className="flexColCenter stat">
-              <span>
-                <CountUp start={1950} end={2000} duration={4} /> <span>+</span>
+            <div className="flexColCenter">
+              <span className="text-2xl sm:text-4xl">
+                <CountUp start={500} end={700} duration={4} />
+                <span className="text-orange-400">+</span>
               </span>
               <span className="secondaryText">Happy Customer</span>
             </div>
-
-            <div className="flexColCenter stat">
-              <span>
-                <CountUp end={28} /> <span>+</span>
+            <div className="flexColCenter">
+              <span className="text-2xl sm:text-4xl">
+                <CountUp start={1} end={11} duration={4} />
+                <span className="text-orange-400">+</span>
               </span>
               <span className="secondaryText">Awards Winning</span>
             </div>
           </div>
         </div>
-
-        {/* right side */}
-        <div className="flexCenter hero-right">
-          <motion.div
-            initial={{ x: "7rem", opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{
-              duration: 2,
-              type: "ease-in",
-            }}
-            className="image-container"
-          >
-            <img src="./hero-image.png" alt="houses" />
-          </motion.div>
-        </div>
+        <motion.div
+        initial={{x:"7rem" ,opacity: 0 }}
+        animate={{x:0,opacity:1}}
+        transition={{duration:2, type:"spring"}}
+         className="flexCenter w-[95%] h-[400px] sm:w-[480px] sm:h-[560px] overflow-hidden  rounded-t-full border-8 border-solid border-neutral-800 ">
+          <img src="./hero-image.png" alt="" className="w-[100%] h-[100%] " />
+        </motion.div>
       </div>
     </section>
   );
