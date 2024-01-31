6ba86e2a280025e20bf067df12c9c256e95b0a8f
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-01-31 11:24:16 +0100

    testing

--- client side/src/App.jsx
+++ client side/src/App.jsx
@@ -1,24 +1,8 @@
-import Carousel from "./components/Carousel";
-import Companies from "./components/Companies";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Value from "./components/Value";
+
+import Website from "./pages/Website";
 function App() {
   return (
-    <div className="app relative overflow-clip">
-      <div>
-        <Header />
-        <div className="white-gradient " />
-        <Hero />
-      </div>
-      <Companies />
-      <Carousel />
-      <Value />
-      <Contact />
-      <Footer />
-    </div>
+    <Website/>
   );
 }
 
---
+++ client side/src/pages/Website.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Carousel from "../components/Carousel";
+import Companies from "../components/Companies";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Value from "../components/Value";
+const Website = () => {
+  return (
+    <div className="app relative overflow-clip">
+    <div>
+      <Header />
+      <div className="white-gradient " />
+      <Hero />
+    </div>
+    <Companies />
+    <Carousel />
+    <Value />
+    <Contact />
+    <Footer />
+  </div>
+  )
+}
+
+export default Website
